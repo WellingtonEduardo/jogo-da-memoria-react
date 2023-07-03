7fda25d9d42b0f399c3655111d74257345b5520a
WellingtonEduardo <wellingtoneduardo10@outlook.com>
2023-07-03 14:51:10 -0300

    first commit

---
+++ src/components/GameOver.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+import React from 'react';
+
+function GameOver(props) {
+
+    return (props.show ?
+        <div id="gameOver">
+            <div>
+                parabéns, você completou o jogo!
+            </div>
+
+            <button id="restart" onClick={props.handleRestart}>jogar novamente</button>
+        </div> : <Fragment />
+    )
+
+}
+
+
+export default GameOver
